778f69e1425b856a9334c971b3a940e7f328fb40
Wiktor <wiktor_wrobel@wp.pl>
2022-12-06 02:46:51 +0100

    changes

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,7 @@ router.post("/signup", (req, res) => {
     if (!err) {
       if (result.length <= 0) {
         query =
-          "insert into user(name, email, password, role) values(?,?,?,'user')";
+          "insert into user(name,email,password,role) values(?,?,?,'user')";
 
         connection.query(
           query,
@@ -23,7 +23,7 @@ router.post("/signup", (req, res) => {
           (err, results) => {
             if (!err) {
               return res
-                .status(500)
+                .status(200)
                 .json({ message: "Successfully registered" });
             } else {
               return res.status(500).json(err);
@@ -44,12 +44,12 @@ router.post("/login", (req, res) => {
   query = "select email,password,role from user where email=?";
   connection.query(query, [user.email], (err, results) => {
     if (!err) {
-      if (results.length <= 0 || results[0].password != user.password) {
+      if (results.length <= 0 || results[0].password !=user.password) {
         return res
           .status(401)
           .json({ message: "Incorrect username or password" });
       } else if (results[0].password == user.password) {
-        const response = { email: results[0].email };
+        const response = { email: results[0].email, role: results[0].role };
         const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
           expiresIn: "8h",
         });
@@ -65,49 +65,52 @@ router.post("/login", (req, res) => {
   });
 });
 
-// var transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD,
-//   },
-// });
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
 
-// router.post("/forgotPassword", (res, req) => {
-//   const user = req.body;
-//   query = "select email,password from user where email=?";
-//   connection.query(query, [user.email], (err, results) => {
-//     if (!err) {
-//       if (results.length <= 0) {
-//         return res
-//           .status(200)
-//           .json({ message: "Password send successfully to your email" });
-//       } else {
-//         var mailOptions = {
-//           from: process.env.EMAIL,
-//           to: results[0].email,
-//           subject: "Password by Powerlifting Plan Generator",
-//           html:
-//             "<p><b>Your login details for Powerlifting Plan Generator</b><br><b>Email: </b>" +
-//             results[0].email +
-//             "<br><b>Password: </b>" +
-//             results[0].password +
-//             '<br><a href="http://localhost:4200/">Click here to login</a></p>',
-//         };
-//         transporter.sendMail(mailOptions, function (error, info) {
-//           if (error) {
-//             console.log(error);
-//           } else {
-//             console.log("Email send: " + info.response);
-//           }
-//         });
-//         return res
-//           .status(200)
-//           .json({ message: "Password send successfully to your email" });
-//       }
-//     }
-//   });
-// });
+router.post("/forgotPassword", (req, res) => {
+  const user = req.body;
+  query = "select email,password from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0) {
+        return res
+          .status(200)
+          .json({ message: "Password send successfully to your email" });
+      } else {
+        var mailOptions = {
+          from: process.env.EMAIL,
+          to: results[0].email,
+          subject: "Password by MaxYourLifts",
+          html:
+            "<p><b>Your login details for MaxYourLifts</b><br><b>Email: </b>" +
+            results[0].email +
+            "<br><b>Password: </b>" +
+            results[0].password +
+            '<br><a href="http://localhost:4200/">Click here to login</a></p>',
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email send: " + info.response);
+          }
+        });
+        return res
+          .status(200)
+          .json({ message: "Password send successfully to your email" });
+      }
+    }
+    else {
+      return res.status(500).json(err)
+    }
+  });
+});
 
 router.get("/get", auth.authenticateToken, checkRole.checkRole, (req, res) => {
   var query = "select id,name,email from user where role='user'";
