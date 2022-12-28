15853eca8188d5aadf232df4a29c531e7cb80b8e
Wiktor <wiktor_wrobel@wp.pl>
2022-12-28 15:16:45 +0100

    changes

--- routes/exercise.js
+++ routes/exercise.js
@@ -10,13 +10,13 @@ router.post(
   checkRole.checkRole,
   (req, res, next) => {
     let exercise = req.body;
-    const categoryId = exercise.categoryId
+    const category_id = exercise.category_id
     const sets = exercise.sets
     const reps = exercise.reps
     const rpe = exercise.rpe
     const tempo = exercise.tempo
-    console.log(categoryId, sets, reps, rpe, tempo)
-    console.log(exercise.categoryId)
+    console.log(category_id, sets, reps, rpe, tempo)
+    console.log(exercise.category_id)
     const exerciseQuery = "INSERT INTO exercises (name, description, video_url, status) VALUES (?, ?, ?, 'true')"
 
     connection.query(exerciseQuery, [exercise.name, exercise.description, exercise.video_url], (error, results) => {
@@ -26,7 +26,7 @@ router.post(
       console.log(`Successfully inserted ${exercise.name} into the exercise table`);
 
       const categoryQuery = 'INSERT INTO exercise_categories (exercise_id, category_id) VALUES ?'
-      const values = categoryId.map(id => [exerciseId, id])
+      const values = category_id.map(id => [exerciseId, id])
       connection.query(categoryQuery, [values], (error, results) => {
         if (error) return res.status(500).json(error);
         console.log(`Successfully assigned the category "id" to ${exercise.name}`);
@@ -51,7 +51,7 @@ router.post(
 
 
 router.get("/get", auth.authenticateToken, (req, res, next) => {
-  const exerciseQuery = "SELECT e.*, c.name as category_name, d.* FROM exercises e JOIN exercise_categories ec ON e.id = ec.exercise_id JOIN categories c ON ec.category_id = c.id JOIN exercise_details d ON e.id = d.exercise_id";
+  const exerciseQuery = "SELECT e.*, c.name as category_name, ec.category_id, d.* FROM exercises e JOIN exercise_categories ec ON e.id = ec.exercise_id JOIN categories c ON ec.category_id = c.id JOIN exercise_details d ON e.id = d.exercise_id";
 
   connection.query(exerciseQuery, (error, results) => {
     if (error) return res.status(500).json(error);
@@ -60,149 +60,169 @@ router.get("/get", auth.authenticateToken, (req, res, next) => {
   });
 });
 
-// router.get("/get", auth.authenticateToken, (req, res, next) => {
-//   var query =
-//     "select e.id, e.name, e.description, e.videoUrl, e.status, c.id as categoryId, c.name as categoryName from exercises as e INNER JOIN categories as c where e.categoryId = c.id";
-//   connection.query(query, (err, results) => {
-//     if (!err) {
-//       return res.status(200).json(results);
-//     } else {
-//       return res.status(500).json(err);
-//     }
-//   });
-// });
-
-// router.get("/getByCategory/:id", auth.authenticateToken, (req, res, next) => {
-//   const id = req.params.id;
-//   var query =
-//     "select id,name from exercises where categoryId= ? and status= 'true'";
-//   connection.query(query, [id], (err, results) => {
-//     if (!err) {
-//       return res.status(200).json(results);
-//     } else {
-//       return res.status(500).json(err);
-//     }
-//   });
-// });
-
 router.get("/getByCategory/:id", auth.authenticateToken, (req, res, next) => {
-  const exerciseId = req.params.id;
-  const exerciseQuery = "SELECT e.*, c.name as category_name, d.* FROM exercises e JOIN exercise_categories ec ON e.id = ec.exercise_id JOIN categories c ON ec.category_id = c.id JOIN exercise_details d ON e.id = d.exercise_id WHERE e.id = ? AND e.status = 'true'";
+  const id = req.params.id;
+  const exerciseQuery = "SELECT e.*, c.name as category_name, d.* FROM exercises e JOIN exercise_categories ec ON e.id = ec.exercise_id JOIN categories c ON ec.category_id = c.id JOIN exercise_details d ON e.id = d.exercise_id WHERE ec.category_id = ? AND e.status = 'true'";
 
-  connection.query(exerciseQuery, [exerciseId], (error, results) => {
+  connection.query(exerciseQuery, [id], (error, results) => {
     if (error) return res.status(500).json(error);
-    console.log(`Successfully retrieved exercise with ID ${exerciseId} from the database`);
-    return res.status(200).json(results[0]);
+    console.log(`Successfully retrieved exercises with category ID ${id} from the database`);
+    return res.status(200).json(results);
   });
 });
 
 
 
-
-// router.get("/getById/:id", auth.authenticateToken, (req, res, next) => {
-//   const id = req.params.id;
-//   var query = "select id,name,description,videoUrl from exercise where id = ?";
-//   connection.query(query, [id], (err, results) => {
-//     if (!err) {
-//       return res.status(200).json(results[0]);
-//     } else {
-//       return res.status(500).json(err);
-//     }
-//   });
-// });
 
 router.get("/getById/:id", auth.authenticateToken, (req, res, next) => {
   const exerciseId = req.params.id;
-  const exerciseQuery = "SELECT e.*, c.name as category_name, d.* FROM exercises e JOIN exercise_categories ec ON e.id = ec.exercise_id JOIN categories c ON ec.category_id = c.id JOIN exercise_details d ON e.id = d.exercise_id WHERE e.id = ?";
-
+  const exerciseQuery = "SELECT c.name as category_name, c.id as category_id FROM categories c JOIN exercise_categories ec ON c.id = ec.category_id WHERE ec.exercise_id = ?";
   connection.query(exerciseQuery, [exerciseId], (error, results) => {
     if (error) return res.status(500).json(error);
-    console.log(`Successfully retrieved exercise with ID ${exerciseId} from the database`);
-    return res.status(200).json(results[0]);
+
+    // Extract the category names and IDs into separate arrays
+    const category_name = results.map(row => row.category_name);
+    const category_id = results.map(row => row.category_id);
+
+    console.log(`Successfully retrieved categories for exercise with ID ${exerciseId} from the database`);
+    return res.status(200).json({ category_name, category_id });
   });
 });
 
-router.patch(
-  "/update",
-  auth.authenticateToken,
-  checkRole.checkRole,
-  (req, res, next) => {
-    let exercise = req.body;
-    var query =
-      "update exercises set name=?,categoryId=?,description=?,videoUrl=? where id=?";
-    connection.query(
-      query,
-      [
-        exercise.name,
-        exercise.categoryId,
-        exercise.description,
-        exercise.videoUrl,
-        exercise.id,
-      ],
-      (err, results) => {
-        if (!err) {
-          if (results.affectedRows == 0) {
-            return res
-              .status(404)
-              .json({ message: "Exercise id does not found" });
-          }
-          return res
-            .status(200)
-            .json({ message: "Exercise updated successfully" });
-        } else {
-          return res.status(500).json(err);
-        }
-      }
-    );
-  }
-);
 
-router.delete(
-  "/delete/:id",
-  auth.authenticateToken,
-  checkRole.checkRole,
-  (req, res, next) => {
-    const id = req.params.id;
-    var query = "delete from exercises where id=?";
-    connection.query(query, [id], (err, results) => {
-      if (!err) {
-        if (results.affectedRows == 0) {
-          return res
-            .status(404)
-            .json({ message: "Exercise id does not found" });
-        }
-        return res
-          .status(200)
-          .json({ message: "Exercise deleted successfully" });
-      } else {
-        return res.status(500).json(err);
-      }
-    });
-  }
-);
 
-router.patch(
-  "/updateStatus",
-  auth.authenticateToken,
-  checkRole.checkRole,
-  (req, res, next) => {
-    let user = req.body;
-    var query = "update exercises set status=? where id=?";
-    connection.query(query, [user.status, user.id], (err, results) => {
-      if (!err) {
-        if (results.affectedRows == 0) {
-          return res
-            .status(404)
-            .json({ message: "Exercise id does not found" });
-        }
-        return res
-          .status(200)
-          .json({ message: "Exercise status updated successfully" });
-      } else {
-        return res.status(500).json(err);
-      }
-    });
+router.patch("/update", auth.authenticateToken, (req, res, next) => {
+  const exerciseId = req.body.id;
+  const exercise = req.body;
+  const category_id = exercise.category_id;
+  const sets = exercise.sets;
+  const reps = exercise.reps;
+  const rpe = exercise.rpe;
+  const tempo = exercise.tempo;
+
+  let exerciseQuery = "UPDATE exercises SET ";
+  let queryParams = [];
+  if (exercise.name) {
+    exerciseQuery += "name = ?, ";
+    queryParams.push(exercise.name);
   }
-);
+  if (exercise.description) {
+    exerciseQuery += "description = ?, ";
+    queryParams.push(exercise.description);
+  }
+  if (exercise.video_url) {
+    exerciseQuery += "video_url = ?, ";
+    queryParams.push(exercise.video_url);
+  }
+  // Remove the trailing comma and space
+  exerciseQuery = exerciseQuery.slice(0, -2);
+  exerciseQuery += " WHERE id = ?";
+  queryParams.push(exerciseId);
+
+  connection.query(exerciseQuery, queryParams, (error, results) => {
+    if (error) return res.status(500).json(error);
+    console.log(`Successfully updated exercise with ID ${exerciseId}`);
+
+    const deleteCategoriesQuery = "DELETE FROM exercise_categories WHERE exercise_id = ?";
+    connection.query(deleteCategoriesQuery, [exerciseId], (error, results) => {
+      if (error) return res.status(500).json(error);
+      console.log(`Successfully deleted categories for exercise with ID ${exerciseId}`);
+
+      const insertCategoriesQuery = "INSERT INTO exercise_categories (exercise_id, category_id) VALUES ?";
+      const values = category_id.map(id => [exerciseId, id]);
+      connection.query(insertCategoriesQuery, [values], (error, results) => {
+        if (error) return res.status(500).json(error);
+        console.log(`Successfully inserted categories for exercise with ID ${exerciseId}`);
+
+        const updateDetailsQuery = "UPDATE exercise_details SET sets = ?, reps = ?, rpe = ?, tempo = ? WHERE exercise_id = ?";
+        connection.query(updateDetailsQuery, [sets, reps, rpe, tempo, exerciseId], (error, results) => {
+          if (error) return res.status(500).json(error);
+          console.log(`Successfully updated details for exercise with ID ${exerciseId}`);
+          return res.status(200).json({ message: "Successfully updated exercise" });
+        });
+      });
+    });
+  });
+});
+
+
+
+
+// router.delete("/delete/:id", auth.authenticateToken, (req, res, next) => {
+//   const exerciseId = req.params.id;
+//   const deleteCategoriesQuery = "DELETE FROM exercise_categories WHERE exercise_id = ?";
+//   const deleteDetailsQuery = "DELETE FROM exercise_details WHERE exercise_id = ?";
+//   const deleteExerciseQuery = "DELETE FROM exercises WHERE id = ?";
+
+//   connection.query(deleteCategoriesQuery, [exerciseId], (error, results) => {
+//     if (error) return res.status(500).json(error);
+//     console.log(`Successfully deleted categories for exercise with ID ${exerciseId} from the database`);
+
+//     connection.query(deleteDetailsQuery, [exerciseId], (error, results) => {
+//       if (error) return res.status(500).json(error);
+//       console.log(`Successfully deleted details for exercise with ID ${exerciseId} from the database`);
+
+//       connection.query(deleteExerciseQuery, [exerciseId], (error, results) => {
+//         if (error) return res.status(500).json(error);
+//         console.log(`Successfully deleted exercise with ID ${exerciseId} from the database`);
+//         return res.status(200).json({ message: "Successfully deleted exercise and related rows" });
+//       });
+//     });
+//   });
+// });
+router.delete("/delete/:id", auth.authenticateToken, (req, res, next) => {
+  const exerciseId = req.params.id;
+  const deleteCategoriesQuery = "DELETE FROM exercise_categories WHERE exercise_id = ?";
+  const deleteDetailsQuery = "DELETE FROM exercise_details WHERE exercise_id = ?";
+  const deleteExerciseQuery = "DELETE FROM exercises WHERE id = ?";
+
+  connection.query(deleteCategoriesQuery, [exerciseId], (error, results) => {
+    if (error) return res.status(500).json(error);
+    console.log(`Successfully deleted categories for exercise with ID ${exerciseId} from the database`);
+
+    connection.query(deleteDetailsQuery, [exerciseId], (error, results) => {
+      if (error) return res.status(500).json(error);
+      console.log(`Successfully deleted details for exercise with ID ${exerciseId} from the database`);
+
+      connection.query(deleteExerciseQuery, [exerciseId], (error, results) => {
+        if (error) return res.status(500).json(error);
+        console.log(`Successfully deleted exercise with ID ${exerciseId} from the database`);
+
+        // Reset the autoincrement value in the exercise table to the last index
+        connection.query("SELECT MAX(id) as max_id FROM exercises", (error, results) => {
+          if (error) return res.status(500).json(error);
+          const maxId = results[0].max_id;
+          connection.query(`ALTER TABLE exercises AUTO_INCREMENT = ${maxId + 1}`, (error, results) => {
+            if (error) return res.status(500).json(error);
+            console.log(`Successfully reset the autoincrement value in the exercise table to ${maxId + 1}`);
+
+            // Reset the autoincrement value in the exercise_details table to the last index
+            connection.query("SELECT MAX(id) as max_id FROM exercise_details", (error, results) => {
+              if (error) return res.status(500).json(error);
+              const maxId = results[0].max_id;
+              connection.query(`ALTER TABLE exercise_details AUTO_INCREMENT = ${maxId + 1}`, (error, results) => {
+                if (error) return res.status(500).json(error);
+                console.log(`Successfully reset the autoincrement value in the exercise_details table to ${maxId + 1}`);
+                return res.status(200).json({ message: "Successfully deleted exercise and related rows" });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
+router.patch("/updateStatus", auth.authenticateToken, (req, res, next) => {
+  const { id, status } = req.body;
+
+  const updateStatusQuery = "UPDATE exercises SET status = ? WHERE id = ?";
+  connection.query(updateStatusQuery, [status, id], (error, results) => {
+    if (error) return res.status(500).json(error);
+    console.log(`Successfully updated status for exercise with ID ${id} in the database`);
+    return res.status(200).json({ message: "Successfully updated exercise status" });
+  });
+});
 
 module.exports = router;
--- routes/exercisePlan.js
+++ routes/exercisePlan.js
@@ -9,59 +9,302 @@ var fs = require("fs");
 var uuid = require("uuid");
 
 router.post("/generatePlan", auth.authenticateToken, (req, res) => {
-  const generatedUuid = uuid.v1();
-  const planDetails = req.body;
-  var exerciseInfoPlan = JSON.parse(planDetails.exerciseInfo);
-  console.log(exerciseInfoPlan);
+  const sql = `SELECT p.name AS plan_name, p.description AS plan_description, pi.sex, pi.age, pi.equipment, pi.weight, pi.squat, pi.bench, pi.deadlift, pi.tested, w.name AS workout_name, w.description AS workout_description, e.id, e.name, e.description, e.video_url, e.status
+  FROM plans p
+  INNER JOIN plan_info pi ON p.id = pi.plan_id
+  INNER JOIN workouts w ON p.id = w.plan_id
+  INNER JOIN workout_exercises we ON w.id = we.workout_id
+  INNER JOIN exercises e ON we.exercise_id = e.id
+  WHERE p.user_id = ? AND p.id = ?`;
+  connection.query(sql, [req.body.userId, req.body.planId], (error, rows) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+      return;
+    }
+    if (rows.length === 0) {
+      res.status(404).json({ message: "Plan not found" });
+      return;
+    }
+    const output = {
+      plan_name: rows[0].plan_name,
+      plan_description: rows[0].plan_description,
+      sex: rows[0].sex,
+      age: rows[0].age,
+      equipment: rows[0].equipment,
+      weight: rows[0].weight,
+      squat: rows[0].squat,
+      bench: rows[0].bench,
+      deadlift: rows[0].deadlift,
+      tested: rows[0].tested,
+      workouts: []
+    };
+    const workouts = {};
+    rows.forEach(row => {
+      if (!workouts[row.workout_name]) {
+        workouts[row.workout_name] = {
+          workout_name: row.workout_name,
+          workout_description: row.workout_description,
+          exercises: []
+        };
+      }
+      workouts[row.workout_name].exercises.push({
+        id: row.id,
+        name: row.name,
+        description: row.description,
+        video_url: row.video_url,
+        status: row.status
+      });
+    });
+    output.workouts = Object.values(workouts);
+    res.json(output);
+  });
+});
 
-  var query =
-    "insert into plan (name,uuid,squat,benchpress,deadlift,exerciseInfo) values(?,?,?,?,?,?)";
-  connection.query(
-    query,
-    [
-      planDetails.name,
-      generatedUuid,
-      planDetails.squat,
-      planDetails.benchpress,
-      planDetails.deadlift,
-      planDetails.exerciseInfo,
-    ],
-    (err, results) => {
-      if (!err) {
-        ejs.renderFile(
-          path.join(__dirname, "", "plan.ejs"),
-          {
-            exerciseInfo: exerciseInfoPlan,
-            name: planDetails.name,
-            squat: planDetails.squat,
-            benchpress: planDetails.benchpress,
-            deadlift: planDetails.deadlift,
-          },
-          (err, results) => {
-            if (err) {
-              return res.status(500).json(err);
-            } else {
-              pdf
-                .create(results)
-                .toFile(
-                  "./generated_pdf/" + generatedUuid + ".pdf",
-                  function (err, data) {
-                    if (err) {
-                      console.log(err);
-                      return res.status(500).json(err);
-                    } else {
-                      return res.status(200).json({ uuid: generatedUuid });
+
+
+router.post("/addPlan", auth.authenticateToken, (req, res) => {
+  const { planName, planDescription, workouts, userId, sex, age, equipment, weight, squat, bench, deadlift, tested } = req.body;
+  connection.beginTransaction(error => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+      return;
+    }
+    connection.query(
+      "INSERT INTO plans (name, description, user_id) VALUES (?, ?, ?)",
+      [planName, planDescription, userId],
+      (error, result) => {
+        if (error) {
+          console.error(error);
+          connection.rollback(() => {
+            res.status(500).json({ message: "Internal server error" });
+          });
+          return;
+        }
+        const planId = result.insertId;
+        connection.query(
+          "INSERT INTO plan_info (plan_id, sex, age, equipment, weight, squat, bench, deadlift, tested) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          [planId, sex, age, equipment, weight, squat, bench, deadlift, tested],
+          (error, result) => {
+            if (error) {
+              console.error(error);
+              connection.rollback(() => {
+                res.status(500).json({ message: "Internal server error" });
+              });
+              return;
+            }
+            const queries = workouts.map(workout =>
+              new Promise((resolve, reject) => {
+                connection.query(
+                  "INSERT INTO workouts (plan_id, name, description) VALUES (?, ?, ?)",
+                  [planId, workout.name, workout.description],
+                  (error, result) => {
+                    if (error) {
+                      console.error(error);
+                      reject();
+                      return;
                     }
+                    const workoutId = result.insertId;
+                    const exerciseQueries = workout.exerciseIds.map(exerciseId =>
+                      new Promise(
+                        (resolve, reject) => {
+                          connection.query(
+                            "INSERT INTO workout_exercises (workout_id, exercise_id) VALUES (?, ?)",
+                            [workoutId, exerciseId],
+                            error => {
+                              if (error) {
+                                console.error(error);
+                                reject();
+                                return;
+                              }
+                              resolve();
+                            }
+                          );
+                        }
+                      )
+                    );
+                    Promise.all(exerciseQueries)
+                      .then(() => {
+                        resolve();
+                      })
+                      .catch(() => {
+                        reject();
+                      });
                   }
                 );
-            }
+              })
+            );
+            Promise.all(queries)
+              .then(() => {
+                connection.commit(error => {
+                  if (error) {
+                    console.error(error);
+                    connection.rollback(() => {
+                      res.status(500).json({ message: "Internal server error" });
+                    });
+                    return;
+                  }
+                  res.json({ message: "Plan added successfully" });
+                });
+              })
+              .catch(() => {
+                connection.rollback(() => {
+                  res.status(500).json({ message: "Internal server error" });
+                });
+              });
           }
         );
-      } else {
-        return res.status(500).json(err);
       }
-    }
-  );
+    );
+  });
 });
+
+
+router.delete("/deletePlan/:id", auth.authenticateToken, (req, res) => {
+  const planId = req.params.id;
+  connection.beginTransaction(error => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+      return;
+    }
+    connection.query("DELETE FROM plan_info WHERE plan_id = ?", [planId], error => {
+      if (error) {
+        console.error(error);
+        connection.rollback(() => {
+          res.status(500).json({ message: "Internal server error" });
+        });
+        return;
+      }
+      connection.query(
+        "SELECT id FROM workouts WHERE plan_id = ?",
+        [planId],
+        (error, rows) => {
+          if (error) {
+            console.error(error);
+            connection.rollback(() => {
+              res.status(500).json({ message: "Internal server error" });
+            });
+            return;
+          }
+          const workoutIds = rows.map(row => row.id);
+          const queries = workoutIds.map(workoutId =>
+            new Promise((resolve, reject) => {
+              connection.query(
+                "DELETE FROM workout_exercises WHERE workout_id = ?",
+                [workoutId],
+                error => {
+                  if (error) {
+                    console.error(error);
+                    reject();
+                    return;
+                  }
+                  resolve();
+                }
+              );
+            })
+          );
+          Promise.all(queries).then(() => {
+            connection.query(
+              "DELETE FROM workouts WHERE plan_id = ?",
+              [planId],
+              error => {
+                if (error) {
+                  console.error(error);
+                  connection.rollback(() => {
+                    res.status(500).json({ message: "Internal server error" });
+                  });
+                  return;
+                }
+                connection.query(
+                  "DELETE FROM plans WHERE id = ?",
+                  [planId],
+                  error => {
+                    if (error) {
+                      console.error(error);
+                      connection.rollback(() => {
+                        res.status(500).json({ message: "Internal server error" });
+                      });
+                      return;
+                    }
+                    connection.commit(error => {
+                      if (error) {
+                        console.error(error);
+                        connection.rollback(() => {
+                          res.status(500).json({ message: "Internal server error" });
+                        });
+                        return;
+                      }
+                      connection.query("SELECT MAX(id) as max_id FROM workouts", (error, results) => {
+                        if (error) return res.status(500).json(error);
+                        const maxId = results[0].max_id;
+                        connection.query(`ALTER TABLE workouts AUTO_INCREMENT = ${maxId + 1}`, (error, results) => {
+                          if (error) return res.status(500).json(error);
+                          console.log(`Successfully reset the autoincrement value in the workouts table to ${maxId + 1}`);
+
+                          // Reset the autoincrement value in the exercise_details table to the last index
+                          connection.query("SELECT MAX(id) as max_id FROM plans", (error, results) => {
+                            if (error) return res.status(500).json(error);
+                            const maxId = results[0].max_id;
+                            connection.query(`ALTER TABLE plans AUTO_INCREMENT = ${maxId + 1}`, (error, results) => {
+                              if (error) return res.status(500).json(error);
+                              console.log(`Successfully reset the autoincrement value in the plans table to ${maxId + 1}`);
+
+                              connection.query("SELECT MAX(id) as max_id FROM plan_info", (error, results) => {
+                                if (error) return res.status(500).json(error);
+                                const maxId = results[0].max_id;
+                                connection.query(`ALTER TABLE plan_info AUTO_INCREMENT = ${maxId + 1}`, (error, results) => {
+                                  if (error) return res.status(500).json(error);
+                                  console.log(`Successfully reset the autoincrement value in the plan_info table to ${maxId + 1}`);
+
+
+
+                                  return res.status(200).json({ message: "Successfully deleted exercise and related rows" });
+                                });
+                              });
+
+                            });
+                          });
+                        });
+                      });
+                    });
+                  }
+                );
+              }
+            );
+          })
+            .catch(() => {
+              connection.rollback(() => {
+                res.status(500).json({ message: "Internal server error" });
+              });
+            });
+        }
+      );
+    });
+  });
+});
+
+router.get("/getPlans", auth.authenticateToken, (req, res) => {
+  const userId = res.locals.userId
+  const sql = `SELECT p.id AS plan_id, p.name AS plan_name, p.description AS plan_description, pi.squat, pi.bench, pi.deadlift
+  FROM plans p
+  INNER JOIN plan_info pi ON p.id = pi.plan_id
+  WHERE p.user_id = ?`;
+  connection.query(sql, [userId], (error, rows) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+      return;
+    }
+    if (rows.length === 0) {
+      res.status(404).json({ message: "No plans found for this user" });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+
 
 module.exports = router;
